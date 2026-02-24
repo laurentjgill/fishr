3f9f085344e348523c93f5963ae972dc5be49270
Lauren Gill <laurengill27@gmail.com>
2026-02-24 10:08:52 -0800

    end of session 1. commiting all files

--- R/cpue.R
+++ R/cpue.R
@@ -1,3 +1,16 @@
+
+#' Calcuate catch per unit effort (CPUE)
+#'
+#' @param catch Numeric vector of catch
+#' @param effort Numeric vector of effort
+#' @param gear_factor Numeric adjustment for gear standard
+#'
+#' @returns A numeric vector of CPUE values
+#' @export
+#'
+#' @examples
+#' cpue(100, 10)
+#' cpue(100, 10, gear_factor = 0.5)
 cpue <- function(catch, effort, gear_factor = 1) {
   raw_cpue <- catch / effort
 
