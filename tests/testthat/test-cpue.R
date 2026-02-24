3d2317b07e2a9b827692f5fd28d5aa3c70a0ad15
Lauren Gill <laurengill27@gmail.com>
2026-02-24 12:22:09 -0800

    new tests

---
+++ tests/testthat/test-cpue.R
@@ -0,0 +1,25 @@
+test_that("cpue calculates simple ratio correctly", {
+  expect_equal(cpue(catch = 100, effort = 10), 10)
+  expect_equal(cpue(catch = 50, effort = 2), 25)
+})
+
+test_that("cpue handles vectors of data", {
+  catches <- c(100, 200, 400)
+  efforts <- c(10, 10, 10)
+  expected_results <- c(10, 20, 40)
+
+  expect_equal(cpue(catches, efforts), expected_results)
+})
+
+test_that("cpue returns numeric values", {
+  expect_type(cpue(100, 10), "double")
+})
+
+test_that("gear factor standardization scales correctly", {
+  expect_equal(cpue(catch = 100, effort = 10, gear_factor = 0.5), 5)
+
+  expect_equal(
+    cpue(catch = 100, effort = 10),
+    cpue(catch = 100, effort = 10, gear_factor = 1)
+  )
+})
