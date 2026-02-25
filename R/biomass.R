40bbb7b6a75f4ca177e4ac71a0177f6bd4a47114
Lauren Gill <laurengill27@gmail.com>
2026-02-25 10:14:09 -0800

    added extra args (cpue to biomass)

--- R/biomass.R
+++ R/biomass.R
@@ -1,7 +1,13 @@
 #' Calculate Biomass Index
 #' Calculate biomass index from CPUE and area swept
+#'
 #' @param cpue Numeric vector of CPUE values
 #' @param area_swept Numeric vector of area swept (eg km squared)
+#' @param catch Optional numeric vector of catch. If provided with `effort`,
+#'   CPUE is computed via `cpue()`.
+#' @param effort Optional numeric vector of effort. Required if `catch` is
+#'   provided.
+#' @param ...
 #'
 #' @returns Numeric vector of biomass index values
 #' @export
@@ -9,6 +15,15 @@
 #' @examples
 #' salmon_cpue <- cpue(catch = 2, effort = 2)
 #' biomass_index(cpue = salmon_cpue, area_swept = 5)
-biomass_index <- function(cpue, area_swept) {
+biomass_index <- function(cpue = NULL,
+                          area_swept,
+                          catch = NULL,
+                          effort = NULL,
+                          ...) {
+#if you have not provided cpue and you have provided catch + effort
+  if (is.null(cpue) && (!is.null(catch) && !is.null(effort))){
+    cpue <- cpue(catch, effort, ...)
+  }
+
   cpue * area_swept
 }
