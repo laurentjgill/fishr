49d0f3c4b8048f5259dbc9429cb9e414859632cb
Lauren Gill <laurengill27@gmail.com>
2026-02-26 11:12:10 -0800

    adding horsefly sample data

---
+++ data-raw/horsefly_sample.R
@@ -0,0 +1,10 @@
+## code to prepare `horsefly_sample` dataset goes here
+
+horsefly_sample <- data.frame(
+  set_number = c(1L, 2L, 3L, 4L, 5L),
+  catch = c(3L, 0L, 5L, 2L, 4L),
+  effort = c(1L, 1L, 1L, 1L, 1L),
+  stringsAsFactors = FALSE
+)
+
+usethis::use_data(horsefly_sample, overwrite = TRUE)
